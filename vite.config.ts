708aa7db3670426b1a3cc6e87c339bbee64dac8e
DanielMarGlez <danielmarglez@gmail.com>
2025-11-30 21:11:24 +0100

    Fix GitHub Pages Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/campflow-waitlist-hub/",   // 👈 CAMBIA ESTO
+  base: "/",   // 👈 CAMBIA ESTO
   server: {
     host: "::",
     port: 8080,
